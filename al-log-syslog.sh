20cea6b6487e3b249108c051870ed0f92d0fe4ec
Ionut Craciunescu <ionut.craciunescu@wealthwizards.com>
2018-08-08 18:34:57 +0100

    privileged=true, remove standalaone host

--- al-log-syslog.sh
+++ al-log-syslog.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -e
 
-/var/alertlogic/lib/remote/bin/al-remote provision \
-    --key $REGISTRATION_KEY \
-    --host-type standalone
-
-/var/alertlogic/lib/remote/bin/al-remote --host-type standalone
+/var/alertlogic/lib/remote/bin/al-remote provision --key $REGISTRATION_KEY
+/var/alertlogic/lib/remote/bin/al-remote
